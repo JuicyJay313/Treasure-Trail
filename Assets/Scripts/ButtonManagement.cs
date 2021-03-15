0acae71f636b21f79476f56a159b8462050bb62d
JuicyJay313 <julienpierg@gmail.com>
2021-03-15 18:22:00 -0400

    Added ButtonManagement script for Main Menu Scene

---
+++ Assets/Scripts/ButtonManagement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ButtonManagement : MonoBehaviour
+{
+    private int highlighted = 0;
+    [SerializeField] TextMeshProUGUI[] buttonTexts;
+    Color32 menuTextColor;
+    void Start()
+    {
+        menuTextColor = buttonTexts[0].color;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("up"))
+        {
+            highlighted = highlighted == 0 ? buttonTexts.Length-1 : highlighted - 1;       
+        }
+
+        if (Input.GetKeyDown("down"))
+        {
+            highlighted = highlighted == buttonTexts.Length-1 ? 0 : highlighted + 1;
+        }
+
+        if (highlighted == 0)
+        {
+            for(int i = 0; i < buttonTexts.Length; i++)
+            {
+                buttonTexts[i].color = menuTextColor;
+            }
+            buttonTexts[highlighted].color = Color.white;
+            
+            if (Input.GetButtonDown("Submit"))
+            {
+                SceneManager.LoadScene("Level 1");
+            }
+        }
+        else if (highlighted == 1)
+        {
+            for (int i = 0; i < buttonTexts.Length; i++)
+            {
+                buttonTexts[i].color = menuTextColor;
+            }
+            buttonTexts[highlighted].color = Color.white;
+
+            if (Input.GetButtonDown("Submit"))
+            {
+                // Go to Credits screen
+            }
+        }
+        else if (highlighted == 2)
+        {
+            for (int i = 0; i < buttonTexts.Length; i++)
+            {
+                buttonTexts[i].color = menuTextColor;
+            }
+            buttonTexts[highlighted].color = Color.white;
+
+            if (Input.GetButtonDown("Submit"))
+            {
+                // Go to Options screen
+            }
+        }
+        
+    }
+
+}
