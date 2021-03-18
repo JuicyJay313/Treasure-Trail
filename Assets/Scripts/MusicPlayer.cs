ef110363ed176ff572c68fc7ea6d254e282dd591
JuicyJay313 <julienpierg@gmail.com>
2021-03-17 21:09:08 -0400

    Added audio related scripts and files

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    AudioSource audioSource;
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+        audioSource = GetComponent<AudioSource>();
+        //audioSource.volume = PlayerPrefsController.GetMasterVolume();
+    }
+
+    public void setVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+}
